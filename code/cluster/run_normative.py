898dbd9e45b27170a5e9b97ffc2828e02629adf0
Linden Parkes <lindenparkes@gmail.com>
2020-04-06 11:48:19 -0400

    posterity

--- code/cluster/run_normative.py
+++ code/cluster/run_normative.py
@@ -7,10 +7,10 @@ from nispat.normative_parallel import execute_nm, collect_nm, delete_nm
 # parallel (batch)
 # ------------------------------------------------------------------------------
 # settings and paths
-python_path = '/home/lindenmp/virtual_env/NormativeNeuroDev_CrossSec_DWI/bin/python'
-normative_path = '/scratch/kg98/Linden/ResProjects/NormativeNeuroDev_CrossSec_DWI/nispat/nispat/normative.py'
-batch_size = 5 # 8 5
-memory = '4G'
+python_path = '/home/lindenmp/virtual_env/nispat/bin/python'
+normative_path = '/home/lindenmp/virtual_env/nispat/nispat/nispat/normative.py'
+batch_size = 16
+memory = '2G'
 duration = '2:00:00'
 cluster_spec = 'm3'
 
@@ -18,12 +18,12 @@ cluster_spec = 'm3'
 # Normative dir
 # ------------------------------------------------------------------------------
 # primary directory for normative model
-exclude_str = 't1Exclude'
 train_test_str = 'squeakycleanExclude'
-combo_label = 'schaefer_200_streamlineCount'
+exclude_str = 't1Exclude'
+combo_label = 'schaefer_200_streamlineCount_consist'
 
-normativedir = os.path.join('/scratch/kg98/Linden/ResProjects/NormativeNeuroDev_CrossSec_DWI/analysis/normative',
-	exclude_str, train_test_str, combo_label, 'ageAtScan1+sex_adj/')
+normativedir = os.path.join('/scratch/kg98/lindenmp/ResProjects/neurodev_cs_predictive/analysis/normative',
+	exclude_str, train_test_str, combo_label, 'ageAtScan1_Years+sex_adj/')
 print(normativedir)
 
 # ------------------------------------------------------------------------------
@@ -40,9 +40,6 @@ resp_test = os.path.join(normativedir, 'resp_test.txt')
 # run normative
 execute_nm(wdir, python_path=python_path, normative_path=normative_path, job_name=job_name, covfile_path=cov_train, respfile_path=resp_train,
            batch_size=batch_size, memory=memory, duration=duration, cluster_spec=cluster_spec, cv_folds=None, testcovfile_path=cov_test, testrespfile_path=resp_test, alg = 'gpr')
-
-collect_nm(wdir, collect=True)
-delete_nm(wdir)
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
@@ -58,24 +55,14 @@ cov_test = os.path.join(wdir, 'synth_cov_test.txt');
 # run normative
 execute_nm(wdir, python_path=python_path, normative_path=normative_path, job_name=job_name, covfile_path=cov_train, respfile_path=resp_train,
            batch_size=batch_size, memory=memory, duration=duration, cluster_spec=cluster_spec, cv_folds=None, testcovfile_path=cov_test, testrespfile_path=None, alg = 'gpr')
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+wdir = normativedir; os.chdir(wdir)
+wdir = os.path.join(normativedir, 'forward/'); os.chdir(wdir)
 
 collect_nm(wdir, collect=True)
 delete_nm(wdir)
-# ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# Cross val model
-job_name = 'cv_'
-wdir = os.path.join(normativedir, 'cv/'); os.chdir(wdir)
-
-# input files and paths
-cov_train = os.path.join(normativedir, 'cov_train.txt')
-resp_train = os.path.join(normativedir, 'resp_train.txt')
-
-# run normative
-execute_nm(wdir, python_path=python_path, normative_path=normative_path, job_name=job_name, covfile_path=cov_train, respfile_path=resp_train,
-           batch_size=batch_size, memory=memory, duration=duration, cluster_spec=cluster_spec, cv_folds=10, testcovfile_path=None, testrespfile_path=None, alg = 'gpr')
-
-collect_nm(wdir, collect=True)
-delete_nm(wdir)
 # ------------------------------------------------------------------------------
