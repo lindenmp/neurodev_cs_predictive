deecc091c999b8a50fcbabc887f7b5177c1549db
Linden Parkes <lindenparkes@gmail.com>
2020-04-14 16:30:13 -0400

    Commented out nuisance regression

--- code/clean_node_metrics.py
+++ code/clean_node_metrics.py
@@ -6,13 +6,20 @@
 # In[1]:
 
 
+# Essentials
 import os, sys, glob
 import pandas as pd
 import numpy as np
+import nibabel as nib
+import scipy.io as sio
+
+# Stats
 import scipy as sp
 from scipy import stats
-import scipy.io as sio
 import statsmodels.api as sm
+import pingouin as pg
+
+# Plotting
 import seaborn as sns
 import matplotlib.pyplot as plt
 plt.rcParams['svg.fonttype'] = 'none'
@@ -21,8 +28,9 @@ plt.rcParams['svg.fonttype'] = 'none'
 # In[2]:
 
 
-sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_CrossSec_DWI/code/func/')
+sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/neurodev_cs_predictive/code/func/')
 from proj_environment import set_proj_env
+sys.path.append('/Users/lindenmp/Dropbox/Work/git/pyfunc/')
 from func import mark_outliers, winsorize_outliers_signed
 
 
@@ -31,13 +39,13 @@ from func import mark_outliers, winsorize_outliers_signed
 
 train_test_str = 'squeakycleanExclude'
 exclude_str = 't1Exclude'
-parc_str = 'schaefer'
+extra_str = '_consist' # '_vol_norm' '_noboxcox'
+edge_weight = 'streamlineCount' # 'streamlineCount' 'fa' 'mean_streamlineLength' 'adc'
 parc_scale = 200
-extra_str = ''
-# extra_str = '_nuis-netdens'
-# extra_str = '_nuis-str'
-parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(train_test_str = train_test_str, exclude_str = exclude_str,
-                                                                            parc_str = parc_str, parc_scale = parc_scale, extra_str = extra_str)
+primary_covariate = 'ageAtScan1_Years'
+parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(exclude_str = exclude_str, train_test_str = train_test_str,
+                                                                                        parc_scale = parc_scale, primary_covariate = primary_covariate,
+                                                                                       extra_str = extra_str, edge_weight = edge_weight)
 
 
 # ### Setup output directory
@@ -45,7 +53,6 @@ parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_p
 # In[4]:
 
 
-print(os.environ['MODELDIR_BASE'])
 print(os.environ['MODELDIR'])
 if not os.path.exists(os.environ['MODELDIR']): os.makedirs(os.environ['MODELDIR'])
 
@@ -55,7 +62,7 @@ if not os.path.exists(os.environ['MODELDIR']): os.makedirs(os.environ['MODELDIR'
 # In[5]:
 
 
-wins_data = True
+wins_data = False
 my_thresh = 3
 norm_data = True
 
@@ -66,7 +73,7 @@ norm_data = True
 
 
 # Load data
-df = pd.read_csv(os.path.join(os.environ['MODELDIR_BASE'], 'df_pheno.csv'))
+df = pd.read_csv(os.path.join(os.environ['MODELDIR'], 'df_pheno.csv'))
 df.set_index(['bblid', 'scanid'], inplace = True)
 print(df.shape)
 
@@ -75,7 +82,7 @@ print(df.shape)
 
 
 # Load data
-df_node = pd.read_csv(os.path.join(os.environ['MODELDIR_BASE'], 'df_node_base.csv'))
+df_node = pd.read_csv(os.path.join(os.environ['MODELDIR'], 'df_node_base.csv'))
 df_node.set_index(['bblid', 'scanid'], inplace = True)
 print(df_node.shape)
 
@@ -89,9 +96,9 @@ df_node.head()
 # In[9]:
 
 
-metrics = ('ct', 'vol', 'str', 'ac', 'mc')
+metrics = ('vol', 'str', 'ac', 'mc')
 dwi_metrics = ('str', 'ac', 'mc')
-t1_metrics = ('ct', 'vol')
+t1_metrics = ('vol',)
 
 
 # ### Compute whole brain averages
@@ -128,7 +135,7 @@ f.ax_joint.collections[0].set_alpha(0)
 
 
 metric_x = 'ageAtScan1'
-metric_y = 'str'
+metric_y = 'ac'
 f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
 f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
@@ -138,12 +145,23 @@ f.ax_joint.collections[0].set_alpha(0)
 # In[14]:
 
 
+metric_x = 'ageAtScan1'
+metric_y = 'mc'
+f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
+f.annotate(sp.stats.pearsonr)
+f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
+f.ax_joint.collections[0].set_alpha(0)
+
+
+# In[15]:
+
+
 metric_x = 'mprage_antsCT_vol_TBV'
 # metric_x = 'network_density'
 # metric_x = 'streamline_count'
 
 
-# In[15]:
+# In[16]:
 
 
 metric_y = 'str'
@@ -153,7 +171,7 @@ f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[16]:
+# In[17]:
 
 
 metric_y = 'ac'
@@ -163,7 +181,7 @@ f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[17]:
+# In[18]:
 
 
 metric_y = 'mc'
@@ -173,7 +191,7 @@ f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[18]:
+# In[19]:
 
 
 for metric in dwi_metrics:
@@ -188,7 +206,7 @@ for metric in dwi_metrics:
 
 # ### Check frequency of outliers
 
-# In[19]:
+# In[20]:
 
 
 df_node_mask = pd.DataFrame(index = df_node.index, columns = df_node.columns)
@@ -208,13 +226,14 @@ for i, metric in enumerate(dwi_metrics):
 
 # ### Winsorize outliers
 
-# In[20]:
+# In[21]:
 
 
 my_str = '|'.join(dwi_metrics); print(my_str)
+# my_str = 'ac|mc'
 
 
-# In[21]:
+# In[22]:
 
 
 if wins_data:
@@ -228,21 +247,20 @@ else:
 
 # ## Normalize
 
-# In[22]:
-
-
-if np.any(df_node<0):
-    print('WARNING: some regional values are <0.. box cox will fail')
-
-
 # In[23]:
 
 
-rank_r = np.zeros(df_node.filter(regex = my_str).shape[1])
+if np.any(df_node < 0):
+    print('WARNING: some regional values are <0, box cox will fail')
+
+if np.any(df_node.drop(train_test_str, axis = 1) == 0):
+    print('WARNING: some regional values are == 0, box cox will fail')
 
 
 # In[24]:
 
+
+rank_r = np.zeros(df_node.filter(regex = my_str).shape[1])
 
 # normalise
 if norm_data:
@@ -253,6 +271,7 @@ if norm_data:
         rank_r[i] = sp.stats.spearmanr(df_node.loc[:,col],x)[0]
         # store normalized version
         df_node.loc[:,col] = x
+    np.sum(rank_r < .99)
 else:
     print('Skipping...')
 
@@ -260,12 +279,32 @@ else:
 # In[25]:
 
 
-np.sum(rank_r < .99)
+# my_str = 'ac|mc'
+
+
+# In[26]:
+
+
+# rank_r = np.zeros(df_node.filter(regex = my_str).shape[1])
+
+# # normalise
+# if norm_data:
+#     for i, col in enumerate(df_node.filter(regex = my_str).columns):
+#         # normalize regional metric
+#         x = sp.stats.yeojohnson(df_node.loc[:,col] + df_node.loc[:,col].abs().max())[0]
+#         # check if rank order is preserved
+#         rank_r[i] = sp.stats.spearmanr(df_node.loc[:,col],x)[0]
+#         # store normalized version
+#         df_node.loc[:,col] = x
+# else:
+#     print('Skipping...')
+
+# np.sum(rank_r < .99)
 
 
 # ### Check distributions
 
-# In[26]:
+# In[27]:
 
 
 f, axes = plt.subplots(2,len(dwi_metrics))
@@ -286,118 +325,109 @@ for i, metric in enumerate(dwi_metrics):
     axes[1,i].set_xlabel(metric+': skewness')
 
 
-# In[27]:
-
-
-my_str = os.environ['MODELDIR'].split('/')[-1]
-my_str = my_str.split('_')[-1]
-my_str
-
-
 # In[28]:
 
 
-if my_str == 'nuis-streamline' or my_str == 'nuis-netdens':
-    df_node = df_node.filter(regex = 'squeakycleanExclude|str|ac|mc', axis = 1)
-elif my_str == 'nuis-str':
-    df_str = df_node.filter(regex = 'str', axis = 1).copy()
-    df_node = df_node.filter(regex = 'squeakycleanExclude|ac|mc', axis = 1)
-else:
-    print('skipping...')
+# my_str = os.environ['MODELDIR'].split('/')[-1]
+# my_str = my_str.split('_')[-1]
+# my_str
 
 
 # In[29]:
 
 
-df_node.shape
+# if my_str == 'nuis-streamline' or my_str == 'nuis-netdens':
+#     df_node = df_node.filter(regex = 'squeakycleanExclude|str|ac|mc', axis = 1)
+# elif my_str == 'nuis-str':
+#     df_str = df_node.filter(regex = 'str', axis = 1).copy()
+#     df_node = df_node.filter(regex = 'squeakycleanExclude|ac|mc', axis = 1)
+# else:
+#     print('skipping...')
 
-
-# ## Nuisance regression
 
 # In[30]:
 
 
-df_node_bak = df_node.copy()
+# df_node.shape
 
+
+# ## Nuisance regression
 
 # In[31]:
 
 
-# labels of nuisance regressors
-nuis = ['averageManualRating']
-print(nuis)
-df_nuis = df[nuis]
-df_nuis = sm.add_constant(df_nuis)
-
-my_str = '|'.join(t1_metrics); print(my_str)
-cols = df_node.filter(regex = my_str, axis = 1).columns
-
-mdl = sm.OLS(df_node.loc[:,cols].astype(float), df_nuis.astype(float)).fit()
-y_pred = mdl.predict(df_nuis)
-y_pred.columns = cols
-df_node.loc[:,cols] = df_node.loc[:,cols] - y_pred
+# df_node_bak = df_node.copy()
 
 
 # In[32]:
 
 
-if my_str == 'nuis-str':
-    print('Running strength nuisance regression')
-    for col in df_node.filter(regex = 'ac|mc', axis = 1).columns:
-        nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS']
-        df_nuis = df[nuis]
-        df_nuis = sm.add_constant(df_nuis)
+# # labels of nuisance regressors
+# nuis = ['averageManualRating']
+# print(nuis)
+# df_nuis = df[nuis]
+# df_nuis = sm.add_constant(df_nuis)
 
-        col_nuis = 'str_' + col.split('_')[1]
-        df_nuis.loc[:,'str'] = df_str.loc[:,col_nuis]
+# my_str = '|'.join(t1_metrics); print(my_str)
+# cols = df_node.filter(regex = my_str, axis = 1).columns
 
-        mdl = sm.OLS(df_node.loc[:,col], df_nuis).fit()
-        y_pred = mdl.predict(df_nuis)
-        df_node.loc[:,col] = df_node.loc[:,col] - y_pred
-else:
-    if my_str == 'nuis-netdens':
-        print('Running network density nuisance regression')
-        nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS', 'network_density']
-        df_nuis = df[nuis]
-    else:
-        print('Running standard nuisance regression')
-        nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS']
-        df_nuis = df[nuis]
-    print(nuis)
-    df_nuis = sm.add_constant(df_nuis)
-
-    my_str = '|'.join(dwi_metrics); print(my_str)
-    cols = df_node.filter(regex = my_str, axis = 1).columns
-
-    mdl = sm.OLS(df_node.loc[:,cols], df_nuis).fit()
-    y_pred = mdl.predict(df_nuis)
-    y_pred.columns = cols
-    df_node.loc[:,cols] = df_node.loc[:,cols] - y_pred
+# mdl = sm.OLS(df_node.loc[:,cols].astype(float), df_nuis.astype(float)).fit()
+# y_pred = mdl.predict(df_nuis)
+# y_pred.columns = cols
+# df_node.loc[:,cols] = df_node.loc[:,cols] - y_pred
 
 
 # In[33]:
 
 
-r = np.zeros(df_node.shape[1])
-for i, col in enumerate(df_node.columns):
-    r[i] = sp.stats.spearmanr(df_node_bak[col],df_node[col])[0]
-sns.distplot(r)
+# if my_str == 'nuis-str':
+#     print('Running strength nuisance regression')
+#     for col in df_node.filter(regex = 'ac|mc', axis = 1).columns:
+#         nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS']
+#         df_nuis = df[nuis]
+#         df_nuis = sm.add_constant(df_nuis)
+
+#         col_nuis = 'str_' + col.split('_')[1]
+#         df_nuis.loc[:,'str'] = df_str.loc[:,col_nuis]
+
+#         mdl = sm.OLS(df_node.loc[:,col], df_nuis).fit()
+#         y_pred = mdl.predict(df_nuis)
+#         df_node.loc[:,col] = df_node.loc[:,col] - y_pred
+# else:
+#     if my_str == 'nuis-netdens':
+#         print('Running network density nuisance regression')
+#         nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS', 'network_density']
+#         df_nuis = df[nuis]
+#     else:
+#         print('Running standard nuisance regression')
+#         nuis = ['mprage_antsCT_vol_TBV', 'dti64MeanRelRMS']
+#         df_nuis = df[nuis]
+#     print(nuis)
+#     df_nuis = sm.add_constant(df_nuis)
+
+#     my_str = '|'.join(dwi_metrics); print(my_str)
+#     cols = df_node.filter(regex = my_str, axis = 1).columns
+
+#     mdl = sm.OLS(df_node.loc[:,cols], df_nuis).fit()
+#     y_pred = mdl.predict(df_nuis)
+#     y_pred.columns = cols
+#     df_node.loc[:,cols] = df_node.loc[:,cols] - y_pred
 
 
 # In[34]:
 
 
-f = sns.jointplot(x = df['ageAtScan1_Years'], y = df_node['str_0'], kind="reg")
-f.annotate(sp.stats.spearmanr)
-# f.annotate(sp.stats.pearsonr)
-f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
-f.ax_joint.collections[0].set_alpha(0)
+# r = np.zeros(df_node.shape[1])
+# for i, col in enumerate(df_node.columns):
+#     r[i] = sp.stats.spearmanr(df_node_bak[col],df_node[col])[0]
+# sns.distplot(r)
 
 
 # In[35]:
 
 
-f = sns.jointplot(x = df['ageAtScan1_Years'], y = df_node['ac_0'], kind="reg")
+idx = df_node.filter(regex = 'vol').corrwith(df[primary_covariate]).abs().argmax()
+f = sns.jointplot(x = df[primary_covariate], y = df_node['vol_'+str(idx)], kind="reg")
 f.annotate(sp.stats.spearmanr)
 # f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
@@ -407,7 +437,8 @@ f.ax_joint.collections[0].set_alpha(0)
 # In[36]:
 
 
-f = sns.jointplot(x = df['ageAtScan1_Years'], y = df_node['mc_0'], kind="reg")
+idx = df_node.filter(regex = 'str').corrwith(df[primary_covariate]).abs().argmax()
+f = sns.jointplot(x = df[primary_covariate], y = df_node['str_'+str(idx)], kind="reg")
 f.annotate(sp.stats.spearmanr)
 # f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
@@ -417,12 +448,34 @@ f.ax_joint.collections[0].set_alpha(0)
 # In[37]:
 
 
+idx = df_node.filter(regex = 'ac').corrwith(df[primary_covariate]).abs().argmax()
+f = sns.jointplot(x = df[primary_covariate], y = df_node['ac_'+str(idx)], kind="reg")
+f.annotate(sp.stats.spearmanr)
+# f.annotate(sp.stats.pearsonr)
+f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
+f.ax_joint.collections[0].set_alpha(0)
+
+
+# In[38]:
+
+
+idx = df_node.filter(regex = 'mc').corrwith(df[primary_covariate]).abs().argmax()
+f = sns.jointplot(x = df[primary_covariate], y = df_node['mc_'+str(idx)], kind="reg")
+f.annotate(sp.stats.spearmanr)
+# f.annotate(sp.stats.pearsonr)
+f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
+f.ax_joint.collections[0].set_alpha(0)
+
+
+# In[39]:
+
+
 df_node.isna().any().any()
 
 
 # ## Save out
 
-# In[38]:
+# In[40]:
 
 
 # Save out
