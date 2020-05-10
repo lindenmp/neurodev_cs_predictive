2c5d136d9b6911b644ba644cfb2f1456037ebcaa
Linden Parkes <lindenparkes@gmail.com>
2020-05-10 10:28:25 -0400

    scoring and permutation testing now includes nuisance regression with OLS

--- code/cluster/predict_pheno_scv.py
+++ code/cluster/predict_pheno_scv.py
@@ -15,7 +15,7 @@ from scipy import stats
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold, GridSearchCV, cross_val_score
-from sklearn.linear_model import Ridge, Lasso
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.svm import SVR, LinearSVR
 from sklearn.metrics import make_scorer, r2_score, mean_squared_error, mean_absolute_error
@@ -25,6 +25,7 @@ from sklearn.metrics import make_scorer, r2_score, mean_squared_error, mean_abso
 parser = argparse.ArgumentParser()
 parser.add_argument("-x", help="IVs", dest="X_file", default=None)
 parser.add_argument("-y", help="DVs", dest="y_file", default=None)
+parser.add_argument("-c", help="DVs", dest="c_file", default=None)
 parser.add_argument("-metric", help="brain feature (e.g., ac)", dest="metric", default=None)
 parser.add_argument("-pheno", help="psychopathology dimension", dest="pheno", default=None)
 parser.add_argument("-seed", help="seed for shuffle_data", dest="seed", default=1)
@@ -36,6 +37,7 @@ args = parser.parse_args()
 print(args)
 X_file = args.X_file
 y_file = args.y_file
+c_file = args.c_file
 metric = args.metric
 pheno = args.pheno
 # seed = int(args.seed)
@@ -77,12 +79,16 @@ def get_reg(num_params = 10):
     return regs, param_grids
 
 
-def get_stratified_cv(X, y, n_splits = 10):
+def get_stratified_cv(X, y, c = None, n_splits = 10):
 
     # sort data on outcome variable in ascending order
     idx = y.sort_values(ascending = True).index
-    X_sort = X.loc[idx,:]
+    if X.ndim == 2: X_sort = X.loc[idx,:]
+    elif X.ndim == 1: X_sort = X.loc[idx]
     y_sort = y.loc[idx]
+    if c is not None:
+        if c.ndim == 2: c_sort = c.loc[idx,:]
+        elif c.ndim == 1: c_sort = c.loc[idx]
     
     # create custom stratified kfold on outcome variable
     my_cv = []
@@ -92,12 +98,58 @@ def get_stratified_cv(X, y, n_splits = 10):
 
         train_idx = np.where(my_bool == False)[0]
         test_idx = np.where(my_bool == True)[0]
-        my_cv.append( (train_idx, test_idx) )  
+        my_cv.append( (train_idx, test_idx) )
 
-    return X_sort, y_sort, my_cv
+    if c is not None:
+        return X_sort, y_sort, my_cv, c_sort
+    else:
+        return X_sort, y_sort, my_cv
 
 
-def run_reg_scv(X, y, reg, param_grid, n_splits = 10, scoring = 'r2', run_perm = False):
+def cross_val_score_nuis(X, y, c, my_cv, reg, my_scorer, c_y = None):
+    
+    accuracy = np.zeros(len(my_cv),)
+
+    for k in np.arange(len(my_cv)):
+        tr = my_cv[k][0]
+        te = my_cv[k][1]
+
+        # Split into train test
+        X_train = X.iloc[tr,:]; X_test = X.iloc[te,:]
+        y_train = y.iloc[tr].values.reshape(-1,1); y_test = y.iloc[te].values.reshape(-1,1)
+        c_train = c.iloc[tr,:]; c_test = c.iloc[te,:]
+        if c_y is not None: c_y_train = c_y.iloc[tr,:]; c_y_test = c_y.iloc[te,:]
+
+        # standardize predictors
+        sc = StandardScaler(); sc.fit(X_train); X_train = sc.transform(X_train); X_test = sc.transform(X_test)
+
+        # standardize covariates
+        sc = StandardScaler(); sc.fit(c_train); c_train = sc.transform(c_train); c_test = sc.transform(c_test)
+        if c_y is not None: sc = StandardScaler(); sc.fit(c_y_train); c_y_train = sc.transform(c_y_train); c_y_test = sc.transform(c_y_test)
+
+        # regress nuisance (X)
+        nuis_reg = LinearRegression(); nuis_reg.fit(c_train, X_train)
+        X_pred = nuis_reg.predict(c_train); X_train = X_train - X_pred
+        X_pred = nuis_reg.predict(c_test); X_test = X_test - X_pred
+
+        # regress nuisance (y)
+        if c_y is None:  
+            nuis_reg = LinearRegression(); nuis_reg.fit(c_train, y_train)
+            y_pred = nuis_reg.predict(c_train); y_train = y_train - y_pred
+            y_pred = nuis_reg.predict(c_test); y_test = y_test - y_pred
+        elif c_y is not None:
+            nuis_reg = LinearRegression(); nuis_reg.fit(c_y_train, y_train)
+            y_pred = nuis_reg.predict(c_y_train); y_train = y_train - y_pred
+            y_pred = nuis_reg.predict(c_y_test); y_test = y_test - y_pred
+
+        reg.fit(X_train, y_train)
+        accuracy[k] = my_scorer(reg, X_test, y_test)
+        
+    return accuracy
+
+
+
+def run_reg_scv(X, y, c, reg, param_grid, n_splits = 10, scoring = 'r2', run_perm = False):
     # NaN check
     if y.isna().any():
         print('Dropping NaNs: ', y.isna().sum())
@@ -107,17 +159,22 @@ def run_reg_scv(X, y, reg, param_grid, n_splits = 10, scoring = 'r2', run_perm =
     pipe = Pipeline(steps=[('standardize', StandardScaler()),
                            ('reg', reg)])
     
-    X_sort, y_sort, my_cv = get_stratified_cv(X, y, n_splits = n_splits)
-    
+    # X_sort, y_sort, my_cv = get_stratified_cv(X, y, n_splits = n_splits)
+    X_sort, y_sort, my_cv, c_sort = get_stratified_cv(X = X, y = y, c = c, n_splits = n_splits)
+
     # if scoring is a dictionary then we run GridSearchCV with multiple scoring metrics and refit using the first one in the dict
-    if type(scoring) == dict: grid = GridSearchCV(pipe, param_grid, cv = my_cv, scoring = scoring, refit = list(scoring.keys())[0])
-    else: grid = GridSearchCV(pipe, param_grid, cv = my_cv, scoring = scoring)
-    
+    grid = GridSearchCV(pipe, param_grid, cv = my_cv, scoring = scoring)
     grid.fit(X_sort, y_sort);
 
-    if run_perm:
-        y_sort.reset_index(drop = True, inplace = True)
+    # rescore with nuisance regression
+    new_reg = copy.deepcopy(reg)
+    if 'reg__alpha' in grid.best_params_: new_reg.alpha = grid.best_params_['reg__alpha']
+    if 'reg__gamma' in grid.best_params_: new_reg.gamma = grid.best_params_['reg__gamma']
+    if 'reg__C' in grid.best_params_: new_reg.C = grid.best_params_['reg__C']
 
+    accuracy_nuis = cross_val_score_nuis(X = X_sort, y = y_sort, c = c_sort, my_cv = my_cv, reg = new_reg, my_scorer = scoring)
+
+    if run_perm:
         null_reg = copy.deepcopy(reg)
         if 'reg__alpha' in grid.best_params_: null_reg.alpha = grid.best_params_['reg__alpha']
         if 'reg__gamma' in grid.best_params_: null_reg.gamma = grid.best_params_['reg__gamma']
@@ -125,22 +182,31 @@ def run_reg_scv(X, y, reg, param_grid, n_splits = 10, scoring = 'r2', run_perm =
 
         pipe = Pipeline(steps=[('standardize', StandardScaler()),
                                ('reg', null_reg)])
+        
+        X_sort.reset_index(drop = True, inplace = True)
+        c_sort.reset_index(drop = True, inplace = True)
 
-        n_perm = 5000
+        n_perm = 1000
         permuted_acc = np.zeros((n_perm,))
+        permuted_acc_nuis = np.zeros((n_perm,))
 
         for i in np.arange(n_perm):
-
             np.random.seed(i)
-            X_perm = X_sort.sample(frac = 1)
-            X_perm.reset_index(inplace = True)
+            idx = np.arange(y_sort.shape[0])
+            np.random.shuffle(idx)
+
+            y_perm = y_sort.iloc[idx]
+            y_perm.reset_index(drop = True, inplace = True)
+            c_y = c_sort.iloc[idx,:]
+            c_y.reset_index(drop = True, inplace = True)
             
-            permuted_acc[i] = cross_val_score(pipe, X_perm, y_sort, scoring = my_scorer, cv = my_cv).mean()
+            permuted_acc[i] = cross_val_score(pipe, X_sort, y_perm, scoring = my_scorer, cv = my_cv).mean()
+            permuted_acc_nuis[i] = cross_val_score_nuis(X = X_sort, y = y_perm, c = c_sort, my_cv = my_cv, reg = null_reg, my_scorer = scoring, c_y = c_y).mean()
 
     if run_perm:
-        return grid, permuted_acc
+        return grid, accuracy_nuis, permuted_acc, permuted_acc_nuis
     else:
-        return grid
+        return grid, accuracy_nuis
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -154,6 +220,9 @@ X = X.filter(regex = metric)
 y = pd.read_csv(y_file)
 y.set_index(['bblid', 'scanid'], inplace = True)
 y = y.loc[:,pheno]
+
+c = pd.read_csv(c_file)
+c.set_index(['bblid', 'scanid'], inplace = True)
 
 # outdir
 outdir = os.path.join(outroot, score, alg + '_' + metric + '_' + pheno)
@@ -172,7 +241,7 @@ elif score == 'mse':
 # prediction
 regs, param_grids = get_reg()
 
-grid, permuted_acc = run_reg_scv(X = X, y = y, reg = regs[alg], param_grid = param_grids[alg], scoring = my_scorer, run_perm = True)
+grid, accuracy_nuis, permuted_acc, permuted_acc_nuis = run_reg_scv(X = X, y = y, c = c, reg = regs[alg], param_grid = param_grids[alg], scoring = my_scorer, run_perm = True)
 # --------------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -182,8 +251,14 @@ f = open(os.path.join(outdir,'best_params.json'),'w')
 f.write(json_data)
 f.close()
 
-np.savetxt(os.path.join(outdir,'best_score.txt'), np.array([grid.best_score_]))
+np.savetxt(os.path.join(outdir,'accuracy_mean.txt'), np.array([grid.cv_results_['mean_test_score'][grid.best_index_]]))
+np.savetxt(os.path.join(outdir,'accuracy_std.txt'), np.array([grid.cv_results_['std_test_score'][grid.best_index_]]))
 np.savetxt(os.path.join(outdir,'permuted_acc.txt'), permuted_acc)
+
+np.savetxt(os.path.join(outdir,'accuracy_nuis.txt'), accuracy_nuis)
+np.savetxt(os.path.join(outdir,'accuracy_mean_nuis.txt'), np.array([accuracy_nuis.mean()]))
+np.savetxt(os.path.join(outdir,'accuracy_std_nuis.txt'), np.array([accuracy_nuis.std()]))
+np.savetxt(os.path.join(outdir,'permuted_acc_nuis.txt'), permuted_acc_nuis)
 
 # --------------------------------------------------------------------------------------------------------------------
 
